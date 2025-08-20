84e986470336d2eac0ee11ab86aeb8032c2dd263
AguirreJM <aguirrejm2406@gmail.com>
2025-08-20 14:46:11 +0800

    config.php

--- my_projects/php/config.php
+++ my_projects/php/config.php
@@ -1,10 +1,11 @@
 <?php 
  
- $con = mysqli_connect("localhost", "root", "", "tutorial");
+ $con = mysqli_connect("localhost", "root", "", "my_project_db");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 
 
 ?>
